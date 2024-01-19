6ed7ed95e77771fddd6bd4247dacd7d917d0b6a9
TealGibbon8 <alsadair.morrison1002@gmail.com>
2024-01-19 14:50:45 +0000

    finished django tutorial part 7

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -26,12 +28,18 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choicetext
+        return self.choice_text
 

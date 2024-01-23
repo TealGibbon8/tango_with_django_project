a8209956e60d699f1418f71c26b8d8b9b9172775
TealGibbon8 <alsadair.morrison1002@gmail.com>
2024-01-23 13:23:19 +0000

    6.2

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0) # default value for views
     likes = models.IntegerField(default=0) # default value for likes
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
     class Meta:
         verbose_name_plural = 'Categories'
 
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import *
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict['categories'] = category_list
 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def details(request):
+    return HttpResponse("Rango says here is the details page.")
